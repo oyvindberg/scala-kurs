9ddfe913b2935c8162eeadce59024ed3e3ebf503
eirmelan <eirik.meland@finn.no>
2013-08-29 15:52:38 +0200

    flere oppgaver knyttet til nummeriske lister

--- oppgaver/collections/src/test/scala/scalakurs/NumeriskeListerTest.scala
+++ oppgaver/collections/src/test/scala/scalakurs/NumeriskeListerTest.scala
@@ -57,6 +57,28 @@ class NumeriskeListerTest
   test("lag en funksjon som returnerer en liste med oddetall") {
     filtrerOddetallLister(List(List(1,2),List(3,4),List(5,7))) should be (List(1,3,5,7))
   }
+
+  test("lag en funksjon som lager partall av oddetall ved a legge til en") {
+    lagPartall(5) should be (6)
+    lagPartall(6) should be (6)
+  }
+
+  test("lag en funksjon som sørger for at alle tall i en liste blir partall") {
+    lagPartallsliste(List(1,2,3,4)) should be (List(2,2,4,4))
+  }
+
+  test("lag en funksjon som finner alle divisorer til et gitt heltall") {
+    divisorer(10) should be (List(1,2,5))
+  }
+
+  test("lag et predikat som sjekker om et tall er et perfekt tall") {
+    perfektTallP(6) should be (true)
+    perfektTallP(10) should be (false)
+  }
+
+  test("finn det fjerde perfekte tallet (6 er det forste)") {
+    (6 to 10000).view.filter(perfektTallP).take(4).last should be (8128)
+  }
 }
 
 class NumeriskeLister {
@@ -81,4 +103,14 @@ class NumeriskeLister {
   def filtrerListerSomKunInneholderOddetall(l: List[List[Int]]): List[List[Int]] = l.filter(alleOddetallP)
 
   def filtrerOddetallLister(l: List[List[Int]]): List[Int] = l.flatten.filter(oddetallP)
+
+  def lagPartall(i: Int): Int = if (oddetallP(i)) i + 1 else i
+
+  def lagPartallsliste(l: List[Int]): List[Int] = l.map(lagPartall)
+
+  def divisorer(i: Int): List[Int] = (1 to i/2).filter(i % _ == 0).toList
+
+  def perfektTallP(i: Int): Boolean = divisorer(i).sum == i
+
+//  def pefektTallNo(no: Int): Int = (6 to 100000)
 }
