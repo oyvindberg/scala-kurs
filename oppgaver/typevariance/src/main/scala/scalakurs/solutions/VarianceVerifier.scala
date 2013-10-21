94654918432c4b5ae0915fa98b155a64f709e7c9
kareblak <kaare.blakstad@gmail.com>
2013-10-21 11:42:50 +0200

    made som pedagocical changes

---
+++ oppgaver/typevariance/src/main/scala/scalakurs/solutions/VarianceVerifier.scala
@@ -0,0 +1,21 @@
+package scalakurs.solutions
+
+import scalakurs._
+
+object VarianceVerifier {
+
+  // Try to implement ImplementedFunction with all possible variance combinations.
+  // Remember that the contained type expected by the Set is _Function[B, B]_,
+  // and the function is defined as Function[-I, +O]!
+  def functions = Set[B => B](
+
+    // Now think variance until the tests turn green!
+    // new ImplementedFunction[???, ???]
+    new ImplementedFunction[B, B],
+    new ImplementedFunction[A, C],
+    new ImplementedFunction[B, C],
+    new ImplementedFunction[A, B]
+  )
+
+}
+
