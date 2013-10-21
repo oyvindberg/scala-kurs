5856ea552f15da482ad3b1d00b903e66a9238cdb
kareblak <kaare.blakstad@gmail.com>
2013-10-21 11:42:31 +0200

    made som pedagocical changes

--- oppgaver/typevariance/src/test/scala/scalakurs/TypeVarianceTest.scala
+++ oppgaver/typevariance/src/test/scala/scalakurs/TypeVarianceTest.scala
@@ -4,11 +4,11 @@ import org.scalatest.FunSuite
 
 class TypeVarianceTest extends FunSuite {
 
-//  import solutions.FunctionClassVerifier._
-  import FunctionClassVerifier._
+//  import solutions.VarianceVerifier._
+  import VarianceVerifier._
 
-  test("Implement all MeaninglessEqualityFunction[I, O] with all valid combinations of classes A, B and C as type parameters I and O") {
-    assert(validBBFunctions.size === 4)
+  test("Implement all ImplementedFunction[I, O] with all valid combinations of classes A, B and C as type parameters I and O") {
+    assert(functions.size === 4)
   }
 
 }
