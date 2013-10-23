cbab2d04c6ce68c9579ab829841ef99cdbac21df
kareblak <kaare.blakstad@gmail.com>
2013-10-23 16:36:34 +0200

    exercises for optionTree

---
+++ oppgaver/option-either/build.sbt
@@ -0,0 +1,8 @@
+name := "option-either"
+
+version := "1.0"
+
+scalaVersion := "2.10.2"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "1.9.1"
+            
---
+++ oppgaver/option-either/src/main/scala/scalakurs/OptionTree.scala
@@ -0,0 +1,9 @@
+package scalakurs
+
+class Tree {
+  def mkString: String = ???
+  def countVertices: Int = ???
+}
+
+case class Node(left: Option[Tree], right: Option[Tree]) extends Tree
+case class Leaf(value: String) extends Tree
