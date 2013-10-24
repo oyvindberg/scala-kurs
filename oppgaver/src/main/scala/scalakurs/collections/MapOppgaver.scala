cbaeb487e3005c514d572e806398d5b9dff3643f
eirmelan <eirik.meland@finn.no>
2013-10-24 15:26:51 +0200

    dokumentasjon og hint

--- oppgaver/src/main/scala/scalakurs/collections/MapOppgaver.scala
+++ oppgaver/src/main/scala/scalakurs/collections/MapOppgaver.scala
@@ -5,14 +5,18 @@ object MapOppgaver {
 
   def leggTilAssosiasjon[K, V](map: Map[K, V], key: K, value: V): Map[K, V] = ???
 
+  // ta bort assosiasjon med gitt key
   def taBortAssosiasjon[K, V](map: Map[K, V], key: K): Map[K, V] = ???
 
+  // ta bort assosiasjon som tilfredstiller predikatet
   def taBortAssosiasjon[K, V](map: Map[K,V], predicate: ((K,V)) => Boolean): Map[K, V] = ???
 
   def brukFunksjonPåAlleVerdier[K, V, VV](map: Map[K, V], f: V => VV): Map[K,VV] = ???
 
+  // hint: withDefaultValue
   def mapMedDefault[K, V](l: List[(K, V)], default: V): Map[K, V] = ???
 
+  // hint: orElse
   def letMedFallbackMap[K, V](key: K, mapA: Map[K, V], mapB: Map[K, V]): V = ???
 }
 
