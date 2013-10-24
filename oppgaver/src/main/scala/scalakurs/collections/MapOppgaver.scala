6b0b89eaca600f206baa0fa19e1a7a72c8c189ec
eirmelan <eirik.meland@finn.no>
2013-10-24 13:49:51 +0200

    Map-oppgaver og løsninger klar til review

--- oppgaver/src/main/scala/scalakurs/collections/MapOppgaver.scala
+++ oppgaver/src/main/scala/scalakurs/collections/MapOppgaver.scala
@@ -1,22 +1,19 @@
 package scalakurs.collections
 
 object MapOppgaver {
-  def nyttMap[K, V](key: K, value: V): Map[K, V] = Map(key -> value)
+  def nyttMap[K, V](key: K, value: V): Map[K, V] = ???
 
-  def leggTilAssosiasjon[K, V](map: Map[K, V], key: K, value: V): Map[K, V] = map + (key -> value)
+  def leggTilAssosiasjon[K, V](map: Map[K, V], key: K, value: V): Map[K, V] = ???
 
-  def taBortAssosiasjon[K, V](map: Map[K, V], key: K): Map[K, V] = map - key
+  def taBortAssosiasjon[K, V](map: Map[K, V], key: K): Map[K, V] = ???
 
-  def taBortAssosiasjon[K, V](map: Map[K,V], predicate: ((K,V)) => Boolean): Map[K, V] =
-    map filterNot predicate
+  def taBortAssosiasjon[K, V](map: Map[K,V], predicate: ((K,V)) => Boolean): Map[K, V] = ???
 
-  def brukFunksjonPåAlleVerdier[K, V, VV](map: Map[K, V], f: V => VV): Map[K,VV] =
-    map.mapValues(f)
+  def brukFunksjonPåAlleVerdier[K, V, VV](map: Map[K, V], f: V => VV): Map[K,VV] = ???
 
-  def mapMedDefault[K, V](l: List[(K, V)], default: V): Map[K, V] =
-    l.toMap.withDefaultValue(default)
+  def mapMedDefault[K, V](l: List[(K, V)], default: V): Map[K, V] = ???
 
-  def letMedFallbackMap[K, V](key: K, mapA: Map[K, V], mapB: Map[K, V]): V = (mapA orElse mapB)(key)
+  def letMedFallbackMap[K, V](key: K, mapA: Map[K, V], mapB: Map[K, V]): V = ???
 }
 
 
