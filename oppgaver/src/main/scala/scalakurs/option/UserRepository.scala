af24fac55c2022aa146f2da848e27b7ea98e587e
eirmelan <eirik.meland@finn.no>
2013-10-25 11:53:30 +0200

    La til Ms. i testene

--- oppgaver/src/main/scala/scalakurs/option/UserRepository.scala
+++ oppgaver/src/main/scala/scalakurs/option/UserRepository.scala
@@ -17,9 +17,10 @@ case class User(id: Int,
 object UserRepository {
 
   private lazy val users = Map(
-    1 -> User(1, "John",    "Doe", 32, Some(Male),   Some(2)),
-    2 -> User(2, "Jane",    "Doe", 30, Some(Female), Some(1)),
-    3 -> User(3, "Doni",    "Doe", 15, None,         None)
+    1 -> User(1, "John",    "Doe",   32, Some(Male),   Some(2)),
+    2 -> User(2, "Jane",    "Doe",   30, Some(Female), Some(1)),
+    3 -> User(3, "Doni",    "Doe",   15, None,         None),
+    4 -> User(4, "Honey",   "Ryder", 27, Some(Female), None)
   )
 
   def findById(id: Int): Option[User] = ???
--- oppgaver/src/main/scala/scalakurs/option/solutions/UserRepository.scala
+++ oppgaver/src/main/scala/scalakurs/option/solutions/UserRepository.scala
@@ -21,10 +21,11 @@ case class User(id: Int,
 
 object UserRepository {
 
-  private val users = Map(
-    1 -> User(1, "John",    "Doe", 32, Some(Male),   Some(2)),
-    2 -> User(2, "Jane",    "Doe", 30, Some(Female), Some(1)),
-    3 -> User(3, "Doni",    "Doe", 15, None,         None)
+  private lazy val users = Map(
+    1 -> User(1, "John",    "Doe",   32, Some(Male),   Some(2)),
+    2 -> User(2, "Jane",    "Doe",   30, Some(Female), Some(1)),
+    3 -> User(3, "Doni",    "Doe",   15, None,         None),
+    4 -> User(4, "Honey",   "Ryder", 27, Some(Female), None)
   )
 
   def findById(id: Int): Option[User] = users.get(id)
