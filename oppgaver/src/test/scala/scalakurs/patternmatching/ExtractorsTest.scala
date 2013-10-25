7653ff44ba3409ae99c4ffcc004b6705082eb196
Stig Henriksen <mail@stighenriksen.com>
2013-10-25 10:46:07 +0200

    Pattern matching-oppgaver: Lag bedre testbeskrivelser.

--- oppgaver/src/test/scala/scalakurs/patternmatching/ExtractorsTest.scala
+++ oppgaver/src/test/scala/scalakurs/patternmatching/ExtractorsTest.scala
@@ -1,19 +1,20 @@
 package scalakurs.patternmatching
 
-import org.scalatest.FunSuite
+import org.scalatest.{FunSpec, FunSuite}
 import org.scalatest.matchers.ShouldMatchers
 
-class ExtractorsTest extends FunSuite with ShouldMatchers {
+class ExtractorsTest extends FunSpec with ShouldMatchers {
 
-  test("Exercise 1: Int.unapply") {
-    assert(Extractors.Int.unapply("1") == Some(1))
-    assert(Extractors.Int.unapply("a") == None)
+  describe("Exercise 1: Int.unapply") {
+    it("(\"1\") should return Some(1)") {assert(Extractors.Int.unapply("1") == Some(1))}
+    it("(\"a\") should return None") {assert(Extractors.Int.unapply("a") == None)}
   }
 
-  test("Exercise 2: PhoneNumber.unapply") {
+  describe("Exercise 2: PhoneNumber.unapply") {
     val prefix = 47
     val number = 22048700
-    assert(Extractors.PhoneNumber.unapply(prefix + " " + number ) == Some((prefix, number)))
-    assert(Extractors.PhoneNumber.unapply("asdf" + " " + number ) == Some(None))
+    it("(\"4722048700\") should return None") {assert(Extractors.PhoneNumber.unapply(prefix.toString + number.toString) == None)}
+    it("(\"47 22048700\") should return" + s"Some(($prefix, $number))") {assert(Extractors.PhoneNumber.unapply(prefix + " " + number ) == Some((prefix, number)))}
+    it("(\"asdf 22048700\") should return Some(None)") {assert(Extractors.PhoneNumber.unapply("asdf" + " " + number) == Some(None))}
   }
 }
--- oppgaver/src/test/scala/scalakurs/patternmatching/PatternMatchingTest.scala
+++ oppgaver/src/test/scala/scalakurs/patternmatching/PatternMatchingTest.scala
@@ -1,56 +1,53 @@
 package scalakurs.patternmatching
 
-import org.scalatest.FunSuite
+import org.scalatest.FunSpec
 import org.scalatest.matchers.ShouldMatchers
 
 import PatternMatching._
 
-class PatternMatchingTest extends FunSuite with ShouldMatchers {
-//  describe("Exercise 1: Match 5") {
-//    it("should match 5") { assert(match5(5))}
-//    it("""should match "5"""") { assert(match5("5"))}
-//    it("should return false for everything else") { assert(!match5("everythingElse"))}
-//  }
+class PatternMatchingTest extends FunSpec with ShouldMatchers {
 
-  test("Exercise 1: Match 5") {
-    assert(match5("5") && match5(5) && !match5(4) && !match5(""))
+  describe("Exercise 1: Match 5") {
+    it("should match 5") { assert(match5(5))}
+    it("""should match "5"""") { assert(match5("5"))}
+    it("should return false for everything else") { assert(!match5("everythingElse"))}
   }
 
-  test("Exercise 2: Match person") {
+  describe("Exercise 2: Extract name and age") {
     val (kobeName, kobeAge, dwyaneName, dwyaneAge) = ("Kobe", 35, "Dwyane", 33)
     val kobe = Person(kobeName, kobeAge)
     val dwyane = Person(dwyaneName, dwyaneAge)
-    assert(extractNameAndAge(kobe) ==(kobeName, kobeAge))
-    assert(extractNameAndAge(dwyane) ==(dwyaneName, dwyaneAge))
+    it(s"should extract ($kobeName, $kobeAge) from Person($kobeName, $kobeAge)") {assert(extractNameAndAge(kobe) ==(kobeName, kobeAge))}
+    it(s"should extract ($dwyaneName, $dwyaneAge) from Person($kobeName, $kobeAge)") {assert(extractNameAndAge(dwyane) ==(dwyaneName, dwyaneAge))}
   }
 
   // Exercise 3
-  test("Exercise 3: Match seq") {
-    assert(extractFirst3Elems(Seq(1, 2, 3, 4)) == Some((1, 2, 3)))
-    assert(extractFirst3Elems(Seq(1, 2, 3)) == Some((1, 2, 3)))
-    assert(extractFirst3Elems(Seq(1, 2)) == None)
-    assert(extractFirst3Elems(Seq()) == None)
+  describe("Exercise 3: Extract first 3 elements") {
+    it("should extract Seq(1,2,3) from Seq(1,2,3,4))") {assert(extractFirst3Elems(Seq(1, 2, 3, 4)) == Some((1, 2, 3)))}
+    it("should extract (Seq(1,2,3) from Seq(1,2,3)") {assert(extractFirst3Elems(Seq(1, 2, 3)) == Some((1, 2, 3)))}
+    it("should return none when trying to extract from Seq(1,2)") {assert(extractFirst3Elems(Seq(1, 2)) == None)}
+    it("should return none when trying to extract from an empty Seq()") {assert(extractFirst3Elems(Seq()) == None)}
   }
 
   private val exampleTree: Tree =
-    Node(5, Node(9, Nil, Node(4, Nil, Nil)), Node(1, Node(12, Nil, Node(11, Nil, Nil)), Nil))
+    Node(5, Node(9, Nil, Node(4, Nil, Nil)), Node(101, Node(12, Nil, Node(11, Nil, Nil)), Nil))
 
   // Exercise 4
-  test("Exercise 4: Check if Tree contains some value v") {
-    assert(contains(exampleTree, 11))
-    assert(!contains(exampleTree, 19))
+  describe("Exercise 4: Check if tree contains v") {
+    it("should return true when v exists in tree") {assert(contains(exampleTree, 11))}
+    it("should return false when v does not exist in tree") {assert(!contains(exampleTree, 19))}
   }
 
   // Exercise 5
-  test("Exercise 5: Sum all values in Tree") {
-    assert(sumTree(exampleTree) == 42)
+  describe("Exercise 5: Sum all values in Tree") {
+    it("should return 142 for test tree") {assert(sumTree(exampleTree) == 142)}
   }
 
   // Exercise 6
-  test("Exercise 6: Check if there's a v for which f(v) is true in Tree") {
-    assert(exists(exampleTree, _ < 5))
-    assert(exists(exampleTree, _ > 100))
-    assert(!exists(exampleTree, _ < 0))
+  describe("Exercise 6: Check if there's a v for which f(v) is true in Tree") {
+    it("should return true for f = _ < 5 since there are several values less than 5 in the test tree") {assert(exists(exampleTree, _ < 5))}
+    it("should return true for f = _ > 100 since there's at least one value in the test tree greater than 100") {assert(exists(exampleTree, _ > 100))}
+    it("should return false for f = _ < 0 since there are no negative values in the test tree") {assert(!exists(exampleTree, _ < 0))}
   }
 
 }
