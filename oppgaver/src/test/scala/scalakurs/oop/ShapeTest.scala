784d9095e649eb022cf62cb959b9fab595e63174
Nina Heitmann Jodal <nina@jodal.no>
2013-10-27 23:25:13 +0100

    Forbedre oppgavene litt

--- oppgaver/src/test/scala/scalakurs/oop/ShapeTest.scala
+++ oppgaver/src/test/scala/scalakurs/oop/ShapeTest.scala
@@ -8,27 +8,36 @@ class ShapeTest extends FunSuite with ShouldMatchers {
 
   val origo = Point(0,0)
 
-  // 1: Comment in draw-method. Make this compile and run green
   test("circle with radius 2") {
     val circle = new Circle(origo, 2)
-//    circle.draw should not be ""
     circle.area should be (12.56 plusOrMinus 0.2)
   }
 
-  // 2: Implement rectangle and make the tests pass
+/** Oppgave 1: Implementer rectangle slik at testen kjører **/
+
 //  test("a rectangle with height of 4 and width of 6") {
 //    val rectangle = new Rectangle(origo, height = 4, width = 6)
 //    rectangle.area should be (24)
 //    rectangle.circumference should be (20)
-//    rectangle.draw should not be ""
 //  }
-//
+
+/** Oppgave 2: Implementer square (arv fra rectangle) slik at testen kjører  **/
+
 //  // 3: Implement square by extending rectangle, and make the tests pass
 //  test("a square with sides of 4") {
 //    val square = new Square(origo, length = 4)
 //    square.area should be (16)
 //    square.circumference should be (16)
-//    square.draw should not be ""
+//  }
+
+/** Oppgave 3 - "tegn" alle shapes slik at testen kjører **/
+
+//  test("Draw elements to screen") {
+//    val circle = new Circle(origo, 5)
+//    val square = new Square(origo, length = 4)
+//    val rectangle = new Rectangle(origo, height = 4, width = 6)
+//    val list = List(circle, square, rectangle)
+//    Shapes.draw(list)
 //  }
 
 }
