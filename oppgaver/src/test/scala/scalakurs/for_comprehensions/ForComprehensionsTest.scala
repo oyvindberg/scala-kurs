95bf91c19adbeb7ad84460f37f7f2ece2da566d7
eirmelan <eirik.meland@finn.no>
2013-10-28 21:44:17 +0100

    inlinet vals som skulle gjøre det lettere å switche mellom besvarelse og løsning

--- oppgaver/src/test/scala/scalakurs/for_comprehensions/ForComprehensionsTest.scala
+++ oppgaver/src/test/scala/scalakurs/for_comprehensions/ForComprehensionsTest.scala
@@ -3,16 +3,14 @@ package scalakurs.for_comprehensions
 import org.scalatest.FunSuite
 
 class ForComprehensionsTest extends FunSuite {
-  val forComprehensions = ForComprehensions
-
   test("finn alle faktorer av x") {
-    assert(forComprehensions.alleFaktorerAv(10) === Seq(1,2,5,10))
-    assert(forComprehensions.alleFaktorerAv(13) === Seq(1, 13))
-    assert(forComprehensions.alleFaktorerAv(98) === Seq(1, 2, 7, 14, 49, 98))
+    assert(ForComprehensions.alleFaktorerAv(10) === Seq(1,2,5,10))
+    assert(ForComprehensions.alleFaktorerAv(13) === Seq(1, 13))
+    assert(ForComprehensions.alleFaktorerAv(98) === Seq(1, 2, 7, 14, 49, 98))
   }
 
   test("finn alle pytagoreiske tripler der a < b < c < 100 (vha brute force)") {
-    val tripler: Seq[(Int, Int, Int)] = forComprehensions.pytagoreiskeTripler
+    val tripler: Seq[(Int, Int, Int)] = ForComprehensions.pytagoreiskeTripler
 
     assert(tripler.size === 52)
     assert(tripler.head === (3, 4, 5))
