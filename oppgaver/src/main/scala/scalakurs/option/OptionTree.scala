52afb3b67e821fa15d5d8f11a8ff302872a6d49d
eirmelan <eirik.meland@finn.no>
2013-10-31 09:34:39 +0100

    fix for compiler error

--- oppgaver/src/main/scala/scalakurs/option/OptionTree.scala
+++ oppgaver/src/main/scala/scalakurs/option/OptionTree.scala
@@ -3,7 +3,7 @@ package scalakurs.option
 class Tree {
   lazy val rootValue: Option[String] = ???
   lazy val mkString: String = ???
-  lazy val nrOfEdges: Int =
+  lazy val nrOfEdges: Int = ???
 }
 
 case class Node(left: Option[Tree], right: Option[Tree]) extends Tree
